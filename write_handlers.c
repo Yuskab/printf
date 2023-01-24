7c958928500739e6579a5a36ad45425fd9261b0f
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-01-24 22:19:02 +0200

    updated

--- write_handlers.c
+++ write_handlers.c
@@ -129,7 +129,6 @@ int write_num(int ind, char buffer[],
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
 			return (write(1, &buffer[padd_start], i - padd_start) +
-
 					write(1, &buffer[ind], length - (1 - padd_start)));
 		}
 	}
